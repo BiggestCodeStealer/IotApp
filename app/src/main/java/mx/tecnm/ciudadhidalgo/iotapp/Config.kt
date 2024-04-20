ceb40e5200404973b508ff6e6b9dd76491bbfd5e
BiggestCodeStealer <142375268+BiggestCodeStealer@users.noreply.github.com>
2024-04-20 17:25:15 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/ciudadhidalgo/iotapp/Config.kt
@@ -0,0 +1,8 @@
+package mx.tecnm.ciudadhidalgo.iotapp
+
+class Config {
+    companion object{
+        //val URL1: String = "http://172.16.32.94/iotrest1/public/"
+        val URL: String = "https://iotrest.itsch.kyared.com/"
+    }
+}
